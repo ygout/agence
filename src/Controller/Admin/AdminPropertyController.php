604741db001ad8a8ceb2185abcef79b47b4c2b97
ygout <yohann.goutaret@gmail.com>
2018-12-10 21:05:56 +0100

    add property and delete

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -42,7 +42,34 @@
      }
 
      /**
-      * @Route("/admin/{id}", name="admin.property.edit")
+      * @Route("/admin/property/create", name="admin.property.new")
+      * @param Property $property
+      * @return Response
+      */
+     public function new(Request $request): Response
+     {
+         $property = new Property();
+         $form = $this->createForm(PropertyType::class, $property);
+         $form->handleRequest($request);
+
+         // Valid form
+         if($form->isSubmitted() && $form->isValid())
+         {
+
+             $this->em->persist($property);
+             $this->em->flush();
+             $this->addFlash('success','Bien ajouté avec succès');
+             return $this->redirectToRoute('admin.property.index');
+         }
+
+         return $this->render('admin/property/new.html.twig', [
+             'property' => $property,
+             'form'     => $form->createView()
+         ]);
+
+     }
+     /**
+      * @Route("/admin/property/{id}", name="admin.property.edit", methods="GET|POST")
       * @param Property $property
       * @return Response
       */
@@ -54,11 +81,31 @@
          if($form->isSubmitted() && $form->isValid())
          {
             $this->em->flush();
+            $this->addFlash('success','Bien modifié avec succès');
             return $this->redirectToRoute('admin.property.index');
          }
          return $this->render('admin/property/edit.html.twig', [
              'property' => $property,
              'form'     => $form->createView()
          ]);
+     }
+
+     /**
+      * @Route("/admin/property/{id}", name="admin.property.delete", methods="DELETE")
+      * @param Property $property
+      * @return Response
+      */
+     public function delete(Property $property, Request $request): Response
+     {
+
+         if($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))){
+
+             $this->em->remove($property);
+             $this->em->flush();
+             $this->addFlash('success','Bien supprimé avec succès');
+         }
+
+        return $this->redirectToRoute('admin.property.index');
+
      }
  }
