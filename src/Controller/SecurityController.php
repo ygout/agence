e8f1791a6d72458d77fb91bb5b2a9ac877b25f28
ygout <yohann.goutaret@gmail.com>
2018-12-14 17:13:04 +0100

    security

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,15 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route('/login', name='login')
+     */
+    public function login()
+    {
+        return $this->render('security/login.html.twig');
+    }
+}
